fcbf14e33a75baeaa3aa40a38bcd1f615260ce5c
zhangtongchuan <zhangtch@yonyou.com>
2018-01-02 15:39:25 +0800

    update

---
+++ mykoanuxt/layouts/less.vue
@@ -0,0 +1,14 @@
+<template>
+<div class="less">
+  <nuxt/>
+</div>
+</template>
+<script>
+import '~/less/less.less'
+export default {
+  name: 'Less',
+  data () {
+    return {}
+  }
+}
+</script>
---
+++ mykoanuxt/pages/less.vue
@@ -0,0 +1,9 @@
+<template>
+  <div></div>
+</template>
+<script>
+export default {
+  layout: 'less',
+  name: 'less'
+}
+</script>
